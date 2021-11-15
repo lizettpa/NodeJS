f89b53cdbd7425d47a6a85a95ad7451331e97177
lizettpa <lizett.pa@hotmail.com>
2021-11-15 12:05:42 -0500

    Se le agregaron nuevas funciones a la creación de un servidor

--- Clase01/httpServer.js
+++ Clase01/httpServer.js
@@ -2,6 +2,7 @@ const { read } = require("fs")
 const http = require("http")
 
 http.createServer((req, res)=>{
+    console.log(req.url)
     res.write("Hola Mundo!!")
     res.end();
 }).listen(8080)
