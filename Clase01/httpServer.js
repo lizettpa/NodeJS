08a4b76db912a578e9ab9a3c5d9898cf4884e8e0
lizettpa <lizett.pa@hotmail.com>
2021-11-15 12:15:31 -0500

    Se copia el archivo del profesor

--- Clase01/httpServer.js
+++ Clase01/httpServer.js
@@ -1,8 +1,14 @@
-const { read } = require("fs")
 const http = require("http")
 
-http.createServer((req, res)=>{
-    console.log(req.url)
-    res.write("Hola Mundo!!")
+http.createServer(function(req,res){
+    let params = req.url.substr(2).split("& ")
+    if(params.length==2){
+        console.log(params)
+        let r = parseFloat(params[0].substr(2))
+        let h = parseFloat(params[1].substr(2))
+        console.log(r,h)
+        let a = (2*Math.PI*(r**2))+(h*(2*Math.PI*r))
+        res.write(`El resultado del área del cilindro es: ${a}`)
+    }
     res.end();
 }).listen(8080)
